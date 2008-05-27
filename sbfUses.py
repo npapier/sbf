a38c4a9c562cdf4b6038eaf27ac027960840aa10
pj.bensoussan <pj.bensoussan@dc778481-724a-0410-bf50-2515cb9dd570>
2008-05-27 08:58:23 +0000

    ~ Updates 'uses' option for Sofa to allow the link with the miniFlowVR library distributed with SOFA.

--- sbfUses.py
+++ sbfUses.py
@@ -207,10 +207,11 @@ def use_sofa( self, lenv, elt ) :
 	lenv['CPPPATH'] += [ os.path.join(sofa_path, 'modules') ]
 	lenv['CPPPATH'] += [ os.path.join(sofa_path, 'framework') ]
 	lenv['CPPPATH'] += [ os.path.join(sofa_path, 'include') ]
+	lenv['CPPPATH'] += [ os.path.join(sofa_path, 'extlibs/miniFlowVR/include') ]
 	lenv['LIBS']	+= ['SofaCore', 'SofaDefaultType', 'SofaComponent', 'SofaHelper', 'SofaSimulation', 'SofaTree', 'SofaAutomateScheduler']
 
 	if self.myPlatform == 'win32' :
-		lenv['LIBS'] += ['libxml2', 'NewMAT', 'Gdi32', 'Shell32']
+		lenv['LIBS'] += ['libxml2', 'miniFlowvR', 'NewMAT', 'Gdi32', 'Shell32']
 		if self.myConfig == 'release' :
 			lenv['LIBPATH']	+= [ os.path.join( sofa_path, 'lib/win32/ReleaseVC8') ]
 		else :
