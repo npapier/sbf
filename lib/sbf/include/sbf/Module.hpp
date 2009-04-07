2285cebbc40bf36e7e181e43d830b09fec14b2f1
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2009-04-07 07:46:48 +0000

    + Adds a new version of Module::getPath() with parameters ordered differently.

--- lib/sbf/include/sbf/Module.hpp
+++ lib/sbf/include/sbf/Module.hpp
@@ -1,7 +1,7 @@
-// SConsBuildFramework - Copyright (C) 2009, Guillaume Brocker.
+// SConsBuildFramework - Copyright (C) 2009, Guillaume Brocker and Nicolas Papier.
 // Distributed under the terms of the GNU General Public License (GPL)
 // as published by the Free Software Foundation.
-// Author Guillaume Brocker
+// Author Guillaume Brocker and Nicolas Papier
 
 #ifndef _SFB_MODULES_HPP_
 #define _SFB_MODULES_HPP_
@@ -35,23 +35,23 @@ struct Module
 	//@{
 	typedef std::vector< const Module * >	Container;
 	typedef Container::const_iterator		ConstIterator;
-	
+
 
 	/**
 	 * @brief	Retrieves the iterator on the registered module collection's begin.
 	 */
 	SBF_API static ConstIterator begin();
-	
+
 	/**
 	 * @brief	Retrieves the iterator on the registered module collection's end.
 	 */
 	SBF_API static ConstIterator end();
-	
+
 	/**
 	 * @brief	Retrieves a registered module by its name or the current program or library if non name is specified.
 	 */
 	SBF_API static const Module & get( const std::string & name = MODULE_NAME );
-	
+
 	/**
 	 * @brief	Retrieves the module for the current program fo library.
 	 */
@@ -60,7 +60,7 @@ struct Module
 		return find( MODULE_NAME );
 	}*/
 	//@}
-	
+
 	/**
 	 * @brief	Builds the module descriptor for the current program or library.
 	 *
@@ -72,13 +72,13 @@ struct Module
 		m_version( MODULE_VERSION )
 	{
 		const Module & found = get(m_name);
-		
+
 		if( ! found )
 		{
 			m_registry.push_back( this );
 		}
 	}
-	
+
 	/**
 	 * @brief	Destructor
 	 */
@@ -103,7 +103,7 @@ struct Module
 	{
 		return m_name;
 	}
-	
+
 	/**
 	 * @brief	Retrieves the version.
 	 */
@@ -120,7 +120,7 @@ struct Module
 		return m_name.empty() == false && m_version.empty() == false;
 	}
 	//@}
-	
+
 	/**
 	 * @name	Path Access
 	 */
@@ -146,11 +146,11 @@ struct Module
 	const boost::filesystem::path getPath( const PathType type ) const
 	{
 		assert( *this && "Cannot retrieve path for an invalid module");
-		
+
 		const boost::filesystem::path	namePath( m_name );
 		const boost::filesystem::path	versionPath( m_version );
 		boost::filesystem::path			basePath;
-		
+
 		basePath = m_initialPath / toString(type);
 		if( boost::filesystem::is_directory(basePath) )
 		{
@@ -172,6 +172,9 @@ struct Module
 	 * @remark	This method does not ensure that the given resource path exists.
 	 *
 	 * @return	an absolute path, or empty if none.
+	 *
+	 * @deprecated
+	 * @todo adds sbfDeprecated() like vgDeprecated()
 	 */
 	const boost::filesystem::path getPath( const boost::filesystem::path & path, const PathType pathType ) const
 	{
@@ -179,8 +182,22 @@ struct Module
 
 		return basePath.empty() ? boost::filesystem::path() : (basePath / path);
 	}
+
+	/**
+	 * @brief	Retrieves the path to a given resource path and for a given type.
+	 *
+	 * @remark	This method does not ensure that the given resource path exists.
+	 *
+	 * @return	an absolute path, or empty if none.
+	 */
+	const boost::filesystem::path getPath( const PathType pathType, const boost::filesystem::path & path ) const
+	{
+		const boost::filesystem::path	basePath = getPath( pathType );
+
+		return basePath.empty() ? boost::filesystem::path() : (basePath / path);
+	}
 	//@}
-	
+
 private:
 
 	SBF_API static Container	m_registry;	///< Holds All registered modules.
@@ -188,13 +205,13 @@ private:
 	const boost::filesystem::path	m_initialPath;	///< Holds the initial module path.
 	const std::string				m_name;			///< Holds the name string of a module.
 	const std::string				m_version;		///< Holds the version string of a module.
-	
+
 	/**
 	 * @brief	Unallowed copy constructor.
 	 */
 	Module( const Module & )
 	{}
-	
+
 	/**
 	 * @brief	Explicit constructor
 	 */
