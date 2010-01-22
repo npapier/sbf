88803196e27c0ead6742c0df9997822142b1f681
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2010-01-22 13:16:30 +0000

    + Adds mkPak for glut. Please note that glut is extracted from sofa framework.

---
+++ pak/mkdb/glut.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# SConsBuildFramework - Copyright (C) 2009, Nicolas Papier.
+# Distributed under the terms of the GNU General Public License (GPL)
+# as published by the Free Software Foundation.
+# Author Nicolas Papier
+
+# ok for clx.y
+import os
+
+sofaUse = UseRepository.getUse('sofa')
+
+print ('INFO: This package exists only as a sofa dependency (this is not a full package).')
+
+descriptor = {
+ 'urls'			: ['http://www.opengl.org/resources/libraries/glut/glut37.zip'],
+
+ 'name'			: 'glut',
+ 'version'		: '3-7',
+
+ 'rootDir'		: 'glut-3.7',
+ 'license'		: ['NOTICE'],
+
+ 'custom'		: [ (os.path.join(sofaUse.getBasePath(), 'bin', 'glut32.dll'), 'lib/') ]
+}
