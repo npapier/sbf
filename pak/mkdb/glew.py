da1bc7cadd4ab207dd68965915bbebb38bb8b84a
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2010-01-28 13:58:58 +0000

    + Adds mkPak for glew. Please note that glut is extracted from sofa framework.

---
+++ pak/mkdb/glew.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# SConsBuildFramework - Copyright (C) 2009, Nicolas Papier.
+# Distributed under the terms of the GNU General Public License (GPL)
+# as published by the Free Software Foundation.
+# Author Nicolas Papier
+
+# ok for clx.y
+import os
+
+sofaUse = UseRepository.getUse('sofa')
+
+print ('INFO: This package exists only as a sofa dependency (this is not a full package).')
+
+descriptor = {
+ 'urls'			: ['http://sourceforge.net/projects/glew/files/glew/1.5.1/glew-1.5.1-src.zip/download'],
+
+ 'name'			: 'glew',
+ 'version'		: '1-5-1',
+
+ 'rootDir'		: 'glew',
+ 'license'		: ['LICENSE.txt'],
+
+ 'custom'		: [ (os.path.join(sofaUse.getBasePath(), 'bin', 'glew32.dll'), 'lib/') ]
+}
