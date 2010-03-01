02e92419ab1a8411f1211d2737672442867c6205
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2010-03-01 15:32:25 +0000

    + Adds mkPak for glm.

---
+++ pak/mkdb/glm.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# SConsBuildFramework - Copyright (C) 2009, Nicolas Papier.
+# Distributed under the terms of the GNU General Public License (GPL)
+# as published by the Free Software Foundation.
+# Author Nicolas Papier
+
+# ok for clx.y
+descriptor = {
+ 'urls'			: ['http://prdownloads.sourceforge.net/glf/glm-0.8.4.1.zip'],
+
+ 'name'			: 'glm',
+ 'version'		: '0-8-4-1',
+
+ 'rootDir'		: 'glm-0.8.4.1',
+ 'license'		: ['copying'],
+ 'include'		: [('glm/', 'glm/')],
+}
