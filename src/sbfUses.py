78d405a3524dd212308aa10f67f71a49a0b9d6bf
Clement.Forest <Clement.Forest@dc778481-724a-0410-bf50-2515cb9dd570>
2010-07-08 10:07:24 +0000

    ~ Updates Use_sofa.

--- src/sbfUses.py
+++ src/sbfUses.py
@@ -869,7 +869,7 @@ class Use_sofa( IUse, sofaConfig ):
 			libs = []
 			pakLibs = ['glew32', 'glut32']
 
-			libsBoth = ['BeamAdapter', 'sofacomponentfem', 'sofacomponentfemfetype', 'sofacomponentfemforcefield', 'sofacomponentfemmaterial', 'sofacomponentfemstraintensor', 'sofaautomatescheduler', 'sofacomponent', 'sofacomponentbase', 'sofacomponentbehaviormodel',
+			libsBoth = ['BeamAdapter', 'sofacomponentfem', 'sofacomponentfemfetype', 'sofacomponentfemforcefield', 'sofacomponentfemmaterial', 'sofacomponentfemstraintensor', 'sofacomponent', 'sofacomponentbase', 'sofacomponentbehaviormodel',
 						'sofacomponentcollision', 'sofacomponentconfigurationsetting', 'sofacomponentconstraintset', 'sofacomponentcontextobject', 'sofacomponentcontroller',
 						'sofacomponentengine', 'sofacomponentfem', 'sofacomponentforcefield', 'sofacomponentinteractionforcefield',
 						'sofacomponentloader', 'sofacomponentlinearsolver', 'sofacomponentmapping', 'sofacomponentmass',
