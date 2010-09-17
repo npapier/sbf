b94341a17c543569ea4fbd71e51ebe7425e4be29
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2010-09-17 10:27:36 +0000

    + Adds mkPak for AMD/ADL_SDK 3.0.

---
+++ pak/mkdb/adl.py
@@ -0,0 +1,19 @@
+# SConsBuildFramework - Copyright (C) 2010, Nicolas Papier.
+# Distributed under the terms of the GNU General Public License (GPL)
+# as published by the Free Software Foundation.
+# Author Nicolas Papier
+
+# ok for clx.y and gccx.y
+
+descriptor = {
+# 'urls'				: ['http://developer.amd.com/Downloads/ADL_SDK_3.0.zip'], this is a 7-zip
+ 'urls'				: ['http://orange/files/Dev/localExt/src/ADL_SDK_3.0.zip'],
+
+ 'name'				: 'adl',
+ 'version'			: '3-0',
+
+ 'rootDir'			: 'ADL_SDK_3.0',
+ 'license'			: ['Public-Documents/ADL SDK EULA.pdf'],
+ 'include'			: ['include/']
+ # no lib
+}
