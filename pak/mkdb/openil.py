8d500ae0864ad43c994ebf5072e46ccc86c2a3cd
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2011-11-21 10:12:33 +0000

    ~ Updates openil mkpak for cl 8, 9 and 10 (Visual C++).

--- pak/mkdb/openil.py
+++ pak/mkdb/openil.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-# SConsBuildFramework - Copyright (C) 2009, 2010, Nicolas Papier.
+# SConsBuildFramework - Copyright (C) 2009, 2010, 2011, Nicolas Papier.
 # Distributed under the terms of the GNU General Public License (GPL)
 # as published by the Free Software Foundation.
 # Author Nicolas Papier
 
-# ok for cl9.0Exp
+# ok for cl8.0Exp, cl9.0Exp, cl10.0Exp
 
 # http://openil.sourceforge.net/
 
@@ -16,20 +16,31 @@ descriptorVersion = '1-7-8'
 projetFolderName = 'devil-1.7.8'
 libPath = 'lib/vc9/x86'
 
-if CCVersionNumber == 9: 
-	vcexpress = r"D:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\VCExpress.exe"
-	sbfPath = os.getenv("SCONS_BUILD_FRAMEWORK")
-	sln = os.path.join( sbfPath, 'pak', 'var', 'build', descriptorName + descriptorVersion, projetFolderName, 'projects', 'msvc9', 'ImageLib.sln' )
-	cmdRelease = "\"{0}\" {1} /build Release /out outRelease.txt".format(vcexpress, sln)
+absRootBuildDir = os.path.join( buildDirectory, descriptorName + descriptorVersion, 'devil-1.7.8', 'projects' )
+
+if CCVersionNumber == 8:
+	descriptor = {
+	 'urls'			: ['http://downloads.sourceforge.net/openil/DevIL-SDK-x86-1.7.8.zip'],
+
+	 'name'			: 'openil',
+	 'version'		: '1-7-8',
+
+	 'license'		: ['COPYING'],
+	 'include'		: ['include/'],
+	 'lib'			: ['lib/*.dll', 'lib/*.lib']
+	}
+elif CCVersionNumber in [9,10]: 
+	sln = os.path.join( absRootBuildDir, 'msvc{0}'.format(int(CCVersionNumber)), 'ImageLib.sln' )
+	cmdRelease = "\"{0}\" {1} /build Release /out outRelease.txt".format(MSVSIDE, sln)
 else:
-	print >>sys.stderr, "Wrong MSVC version. Version 9.0Exp Required."
+	print >>sys.stderr, "Wrong MSVC version. Version 8.0[Exp], 9.0[Exp] or 10.0[Exp] required."
 	exit(1)
 
 descriptor = {
  'urls'			: ['http://downloads.sourceforge.net/openil/DevIL-1.7.8.zip', 'http://orange/files/Dev/localExt/src/openil1-7-8_LibCompiled-vc9.zip', 'http://orange/files/Dev/localExt/src/openil1-7-8_PATCH.zip'],
 
  'rootBuildDir'	: projetFolderName,
- 'builds'		: [	cmdRelease ],
+ 'builds'		: [ cmdRelease ],
 
  'name'			: descriptorName,
  'version'		: descriptorVersion,
@@ -39,15 +50,3 @@ descriptor = {
  'include'		: [('include/IL/','IL/')],
  'lib'			: [ os.path.join(libPath, '*.dll'), os.path.join(libPath, '*.lib') ]
 }
-
-# ok for cl8.y
-# descriptor = {
-# 'urls'			: ['http://downloads.sourceforge.net/openil/DevIL-SDK-x86-1.7.8.zip'],
-
-# 'name'			: 'openil',
-# 'version'		: '1-7-8',
-
-# 'license'		: ['COPYING'],
-# 'include'		: ['include/'],
-# 'lib'			: ['lib/*.dll', 'lib/*.lib']
-#}
