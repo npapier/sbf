3648a21b5dc81aad6c5373699211d302e366d010
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2011-12-07 08:10:52 +0000

    ~ Updates physicsfs mkpak for revision 2-0-2. The library is built now using cmake. MS/cl version 8, 9, 10 and 11 are supported. Adds missing license files for embedded external libraries.

--- pak/mkdb/physicsfs.py
+++ pak/mkdb/physicsfs.py
@@ -1,47 +1,53 @@
 #!/usr/bin/env python
 
-# SConsBuildFramework - Copyright (C) 2010, Nicolas Papier.
+# SConsBuildFramework - Copyright (C) 2010, 2011, Nicolas Papier.
 # Distributed under the terms of the GNU General Public License (GPL)
 # as published by the Free Software Foundation.
 # Author Nicolas Papier
 # Author Maxime Peresson
 
-# cl9-0Exp
+# http://icculus.org/physfs/
+# cl8-0[Exp], cl9-0[Exp], cl10-0[Exp] and cl11-0[Exp]
+# Support for zip (zlib123), 7z (using lzma)
 
 import os.path
 
-# http://icculus.org/physfs/
-# MSVC solution is created with CMake.
+descriptorName = 'physfs'
+descriptorVersion = '2-0-2'
+descriptorVersionWithDot = descriptorVersion.replace('-', '.')
 
-if CCVersionNumber == 9: 
-	vcexpress = r"C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\VCExpress.exe"
-	sbfPath = os.getenv("SCONS_BUILD_FRAMEWORK")
-	projetFolderName = 'physfs2-0-1'
-	sln = os.path.join( sbfPath, 'pak', 'var', 'build', projetFolderName, projetFolderName, 'bin', 'PhysicsFS.sln' )
-	cmdDebug = "\"{0}\" {1} /build Debug /project physfs /out outDebug.txt".format(vcexpress, sln)
-	cmdRelease = "\"{0}\" {1} /build Release /project physfs /out outRelease.txt".format(vcexpress, sln)
-	
+cmakeGenerator = {
+	8	: 'Visual Studio 8 2005',
+	9	: 'Visual Studio 9 2008',
+	10	: 'Visual Studio 10',
+	11	: 'Visual Studio 11' }
+
+if CCVersionNumber in [8, 9, 10, 11]:
+	options = '-D PHYSFS_ARCHIVE_GRP:BOOL=OFF -D PHYSFS_ARCHIVE_HOG:BOOL=OFF -D PHYSFS_ARCHIVE_MVL:BOOL=OFF -D PHYSFS_ARCHIVE_QPAK:BOOL=OFF -D PHYSFS_ARCHIVE_WAD:BOOL=OFF -D PHYSFS_BUILD_STATIC:BOOL=OFF'
+	cmdConfigure = 'cmake -G "{generator}" {options} CMakeLists.txt'.format( generator=cmakeGenerator[CCVersionNumber], options=options )
+	# @todo generates debug version with a different name
+	#cmdDebug = 'cmake --build . --config debug'
+	cmdRelease = 'cmake --build . --config release'
 else:
-	print >>sys.stderr, "Wrong MSVC version. Version 9.0Exp Required."
+	print >>sys.stderr, "Wrong MSVC version. Version 8.0[Exp], 9.0[Exp], 10.0[Exp] or 11.0[Exp] required."
 	exit(1)
 
 descriptor = {
- 'urls'			: [	'http://orange/files/Dev/localExt/src/physfs-2-0-1.zip' ],
+ 'urls'			: [ 'http://icculus.org/physfs/downloads/physfs-2.0.2.tar.gz' ],
 
- 'rootBuildDir'	: 'physfs2-0-1',
- 'builds'		: [	cmdDebug, cmdRelease ],
+ 'rootBuildDir'	: descriptorName + '-' + descriptorVersionWithDot,
+ #'builds'		: [ cmdConfigure, cmdDebug, cmdRelease ],
+ 'builds'		: [ cmdConfigure, cmdRelease ],
 
- 'name'			: 'physfs',
- 'version'		: '2-0-1',
+ 'name'			: descriptorName,
+ 'version'		: descriptorVersion,
 
- 'rootDir'		: 'physfs2-0-1',
+ 'rootDir'		: descriptorName + '-' + descriptorVersionWithDot,
  
- 'license'		: [('LICENSE.txt')],
- 
+ 'license'		: [('LICENSE.txt'), ('CREDITS.txt'), ('lzma/lzma.txt'), ('zlib123/README')],
+
  'include'		: [	'*.h' ],
 
-'lib'			: [	'bin/Release/physfs.lib', 
-					'bin/Release/physfs.dll',
-					'bin/Debug/physfs-d.lib',
-					'bin/Debug/physfs-d.dll' ],
+ 'lib'			: [	'Release/physfs.lib',
+					'Release/physfs.dll' ]
 }
