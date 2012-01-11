52cbd07e9deda27d9de9b7bb9ed63a7247197d93
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2012-01-11 09:04:36 +0000

    + Adds getCC(),getCCVersionNumber() and getPlatformCCPostfix() to retrieve the compiler, the compiler version number and the postfix added by sbf executable and library filenames.

--- lib/sbf/include/sbf/operations.hpp
+++ lib/sbf/include/sbf/operations.hpp
@@ -1,7 +1,8 @@
-// SConsBuildFramework - Copyright (C) 2011, Guillaume Brocker.
+// SConsBuildFramework - Copyright (C) 2011, 2012, Guillaume Brocker, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Guillaume Brocker
+// Author Nicolas Papier
 
 #ifndef _SBF_OPERATIONS_HPP_
 #define _SBF_OPERATIONS_HPP_
@@ -16,15 +17,42 @@ namespace sbf
 
 
 /**
-  * @brief	Retrieves the current platform.
+  * @brief Retrieves the current platform.
+  *
+  * @return the current platform (win32, cygwin, posix or darwin).
   */
 SBF_API const std::string getPlatform();
 
 /**
- * @brief	Retrieves the compiler version used for building.
+ * @brief Retrieves the compiler used for building.
+ *
+ * @return the compiler (cl, gcc).
+ */
+SBF_API const std::string getCC();
+
+
+/**
+ * @brief Retrieves the compiler version number used for building.
+ *
+ * @return the compiler version number (8.0 for MS/cl 8.0, 4.002001 for gcc 4.2.1).
+ */
+SBF_API const double getCCVersionNumber();
+
+
+/**
+ * @brief Retrieves the compiler version used for building.
+ *
+ * @return the compiler version (9-0, 4-2-1)
  */
 SBF_API const std::string getCCVersion();
 
+
+/**
+ * @brief Retrieves the postfix added by sbf to executable and library filenames
+ *
+ * @return the postfix containing platform and compiler version (_win32_cl9-0Exp, _posix_gcc4-2-1)
+ */
+SBF_API const std::string getPlatformCCPostfix();
 
 
 } // namespace sbf
--- lib/sbf/src/operations.cpp
+++ lib/sbf/src/operations.cpp
@@ -1,7 +1,8 @@
-// SConsBuildFramework - Copyright (C) 2011, Guillaume Brocker.
+// SConsBuildFramework - Copyright (C) 2011, 2012, Guillaume Brocker, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Guillaume Brocker
+// Author Nicolas Papier
 
 #include "sbf/operations.hpp"
 
@@ -23,14 +24,43 @@ const std::string getPlatform()
 }
 
 
+const std::string getCC()
+{
+	#ifdef SBF_CC
+		return TOSTRING(SBF_CC);
+	#else
+		#error "Undefined SBF CC";
+	#endif
+}
+
+
+const double getCCVersionNumber()
+{
+	#ifdef SBF_CC_VERSION_NUMBER
+		return SBF_CC_VERSION_NUMBER;
+	#else
+		#error "Undefined SBF cc version number";
+	#endif
+}
+
+
 const std::string getCCVersion()
 {
 	#ifdef SBF_CC_VERSION
 		return TOSTRING(SBF_CC_VERSION);
 	#else
-		#error "Undefined SBF CC version";
+		#error "Undefined SBF cc version";
 	#endif
 }
 
+
+const std::string getPlatformCCPostfix()
+{
+	#ifdef SBF_PLATFORM_CC_VERSION
+		return TOSTRING(SBF_PLATFORM_CC_VERSION);
+	#else
+		#error "Undefined SBF platform and cc postfix";
+	#endif
+}
 
 } // namespace sbf
