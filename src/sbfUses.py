3906f0784a6e4ce4ea6e07b94604fd50ff2ae569
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2012-05-04 07:01:03 +0000

    + Adds optionally DTToolControllers shared library to Use_sofa::getLIBS().

--- src/sbfUses.py
+++ src/sbfUses.py
@@ -932,6 +932,7 @@ class Use_sofa( IUse, sofaConfig ):
 						, 'sofa_object_interaction', 'sofa_rigid', 'sofa_simple_fem', 'sofa_sph_fluid', 'sofa_taucs_solver', 'sofa_topology_mapping', 'sofa_user_interaction', 'sofa_volumetric_data'
 						, 'sofahelper', 'sofagui', 'sofasimulation', 'sofatree', 'TriangularMeshRefiner', 'PersistentContact', 'Suture', 'BeamAdapter' ]
 
+			#libsBoth += [ 'DTToolControllers' ]
 			#libsBoth += [ 'sofa_eigen2_solver' ]
 
 			staticLibs = ['miniFlowVR', 'newmat', 'taucs_mt', 'tinyxml']
