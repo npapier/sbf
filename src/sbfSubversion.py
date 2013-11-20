736255585a2372ce50877ea8e55dbf284d1d1258
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2013-11-20 07:49:55 +0000

    ~ Improves error message.

--- src/sbfSubversion.py
+++ src/sbfSubversion.py
@@ -595,7 +595,7 @@ def svnIsUpdateAvailable( path, desiredRevisionNumber, verbose = True, veryVerbo
 				return False
 	else:
 		# path is not an existing path, an update is available (first checkout).
-		if verbose:	print ( '{project} not found.'.format(project=project.upper()) )
+		if verbose:	print ( '{project} not found.'.format(project=project) )
 		return True
 
 # svnIsUnversioned, svnIsVersioned
