8569ed0af67c1464240696e2fc9cf39810b31be0
guillaume.brocker <guillaume.brocker@dc778481-724a-0410-bf50-2515cb9dd570>
2014-06-12 15:07:22 +0000

    + Adds support for building on posix platforms.

--- pak/mkdb/blowfish.py
+++ pak/mkdb/blowfish.py
@@ -6,6 +6,7 @@
 # ok for cl9.0 and 10.0
 
 from os.path import join
+import sys
 
 # http://www.codeproject.com/KB/security/blowfish.aspx
 
@@ -13,25 +14,41 @@ descriptorName = 'blowfish'
 descriptorVersion = '1-0'
 projectFolderName = ''
 
-if CCVersionNumber >= 9:
-	sln = join( buildDirectory, descriptorName + descriptorVersion, 'Blowfish.sln' )
-	cmdDebug = "\"{0}\" {1} /build Debug /out outDebug.txt".format(MSVSIDE, sln)
-	cmdRelease = "\"{0}\" {1} /build Release /out outRelease.txt".format(MSVSIDE, sln)
-else:
-	print >>sys.stderr, "Unsupported MSVC version."
-	exit(1)
 
+if platform == 'win32':
+	if CCVersionNumber >= 9:
+		sln = join( buildDirectory, descriptorName + descriptorVersion, 'Blowfish.sln' )
+		patchList = [ 'http://orange/files/Dev/localExt/src/Blowfish_{0}_PATCH.zip'.format(CCVersion) ]
+		cmd = [
+			# Release build command
+			"\"{0}\" {1} /build Debug /out outDebug.txt".format(MSVSIDE, sln),
+			# Debug build command
+			 "\"{0}\" {1} /build Release /out outRelease.txt".format(MSVSIDE, sln) ]
+		lib = [ ('Release/Blowfish.lib', 'Blowfish.lib'), ('Debug/Blowfish.lib', 'Blowfish_D.lib') ]
+	else:
+		print >>sys.stderr, "Unsupported MSVC version."
+		exit(1)
+elif platform == 'posix':
+	patchList = [ 'http://orange/files/Dev/localExt/src/Blowfish_{0}_PATCH.zip'.format(CCVersion) ]
+	cmd = [ 'g++ --shared -o libBlowfish.so BlowFish.cpp' ]
+	lib = [ 'libBlowfish.so' ]
+else:
+	print >>sys.stderr, 'Unsupported platform {0}.'.format(platform)
+	exit(1)
+	
+	
 descriptor = {
-	'urls'			: ['http://orange/files/Dev/localExt/src/Blowfish.zip', 'http://orange/files/Dev/localExt/src/Blowfish_{0}_PATCH.zip'.format(CCVersion) ],
+	'urls'			: ['http://orange/files/Dev/localExt/src/Blowfish.zip'] + patchList,
 
 	'name'			: descriptorName,
 	'version'		: descriptorVersion,
+	'license'		: ['COPYING'],
 
 	'rootBuildDir'	: projectFolderName,
-	'builds'		: [ cmdDebug, cmdRelease ],
+	'builds'		: cmd,
 
 	'rootDir'		: projectFolderName,
 	'license'		: ['COPYING'],
 	'include'		: ['Blowfish.h'],
-	'lib'			: [ ('Release/Blowfish.lib', 'Blowfish.lib'), ('Debug/Blowfish.lib', 'Blowfish_D.lib') ]
+	'lib'			: lib
 }
