bb14a4259c61d98ee37de653386bd42af6c5b785
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2014-06-20 08:10:43 +0000

    ~ Updates mkpak for swig and swigShp.

--- pak/mkdb/swig.py
+++ pak/mkdb/swig.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-# SConsBuildFramework - Copyright (C) 2013, Nicolas Papier.
+# SConsBuildFramework - Copyright (C) 2013, 2014, Nicolas Papier.
 # Distributed under the terms of the GNU General Public License (GPL)
 # as published by the Free Software Foundation.
 # Author Nicolas Papier
 
 version = '2-0-12'
+#version = '3-0-2'
 versionDot = version.replace('-', '.')
 
 descriptor = {
--- pak/mkdb/swigShp.py
+++ pak/mkdb/swigShp.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
-# SConsBuildFramework - Copyright (C) 2013, Nicolas Papier.
+# SConsBuildFramework - Copyright (C) 2013, 2014, Nicolas Papier.
 # Distributed under the terms of the GNU General Public License (GPL)
 # as published by the Free Software Foundation.
 # Author Nicolas Papier
 
 version = '2-0-12'
+#version = '3-0-2'
 versionDot = version.replace('-', '.')
 
 descriptor = {
- 'urls'			: [ "http://orange/files/Dev/localExt/src/swigShp{0}.zip".format(version) ],
+ 'urls'			: [ 'http://orange/files/Dev/localExt/PUBLIC/src/swigShp{0}.zip'.format(version) ],
 
  'name'			: 'swigShp',
  'version'		: version,
