f2f8633e5b16c9633e857a4bce3363b92ad10d6b
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2014-09-09 09:38:15 +0000

    ~ Updates gtest mkpak to use new API.

--- pak/mkdb/gtest.py
+++ pak/mkdb/gtest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# SConsBuildFramework - Copyright (C) 2010, 2011, 2013, Nicolas Papier.
+# SConsBuildFramework - Copyright (C) 2010, 2011, 2013, 2014, Nicolas Papier.
 # Distributed under the terms of the GNU General Public License (GPL)
 # as published by the Free Software Foundation.
 # Author Nicolas Papier
@@ -17,25 +17,7 @@ options = ' -D BUILD_SHARED_LIBS:BOOL=ON -D CMAKE_DEBUG_POSTFIX=-d '
 cmakeInitialCacheOption = ' -C "../CMakeInitialCache.txt" '
 
 # CMakeInitialCache.txt
-def createCMakeInitialCacheFile( directory ):
-	def _createCMakeInitialCacheFile( directory ):
-		str = """message("Input: CMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}")
-
-if (CMAKE_CXX_FLAGS)
-	message("Customizing CMAKE_CXX_FLAGS")
-
-	# add new flags
-	set (FLAGS "${CMAKE_CXX_FLAGS} /D _VARIADIC_MAX=10 ")
-
-	set(CMAKE_CXX_FLAGS "${FLAGS}" CACHE STRING "" FORCE)
-	message("Output:CMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}")
-else()
-	message("Don't customize CMAKE_CXX_FLAGS")
-endif()
-"""
-		with open( join(directory, 'CMakeInitialCache.txt'), 'w' ) as f:
-			f.write(str)
-	return lambda : _createCMakeInitialCacheFile(directory)
+cmakeInitialCacheContents = GetCMakeInitialCacheCodeToAppendValue( 'CXX_FLAGS', '/D _VARIADIC_MAX=10' )
 
 descriptor = {
  'svnUrl'		: 'http://googletest.googlecode.com/svn/trunk@{0}'.format(descriptorVersion),
@@ -44,9 +26,9 @@ descriptor = {
  'version'		: descriptorVersion,
 
  'rootBuildDir'	: descriptorName,
- 'builds'		: [ createCMakeInitialCacheFile(join( buildDirectory, descriptorName + descriptorVersion )),
-					getCMakeCmdConfigure(CCVersionNumber, options ),
-					getCMakeCmdConfigure(CCVersionNumber, options + cmakeInitialCacheOption ),
+ 'builds'		: [ CreateCMakeInitialCache(join( buildDirectory, descriptorName + descriptorVersion ), cmakeInitialCacheContents),
+					getCMakeCmdConfigure(CC, CCVersionNumber, arch, options ),
+					getCMakeCmdConfigure(CC, CCVersionNumber, arch, options + cmakeInitialCacheOption ),
 					getCMakeCmdBuildDebug(), getCMakeCmdBuildRelease() ],
 
  'rootDir'		: descriptorName,
