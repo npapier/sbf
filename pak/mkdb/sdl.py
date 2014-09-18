c187c59a7af2abd773e763957f429fc2a5ca4d07
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2014-09-18 12:34:50 +0000

    ~ Updates mkpak for sdl.

--- pak/mkdb/sdl.py
+++ pak/mkdb/sdl.py
@@ -18,28 +18,39 @@ descriptorVersionDot = descriptorVersion.replace('-', '.')
 projectFolderName = 'SDL-{}'.format(descriptorVersionDot)
 
 slnPath = 'VisualC'
-
+vcxprojFiles = []
 if arch == 'x86-32':
 	platform = 'Win32'
 	lib =  [ 'VisualC/Release/SDL.lib', 'VisualC/SDLmain/Release/SDLmain.lib' ]
+	dxguidPath = '$(DXSDK_DIR)/Lib/x86'
 else:
 	platform = 'x64'
 	lib = [ 'VisualC/x64/Release/SDL.lib', 'VisualC/SDLmain/x64/Release/SDLmain.lib' ]
+	dxguidPath = '$(DXSDK_DIR)/Lib/x64'
 
 ConfigureVisualStudioVersion(CCVersionNumber)
+
 cmdSDLRelease = GetMSBuildCommand( slnPath, 'SDL.sln', 'SDL', 'Release', maxcpucount = cpuCount, platform = platform )
 cmdSDLMainRelease = GetMSBuildCommand( slnPath, 'SDL.sln', 'SDLmain', 'Release', maxcpucount = cpuCount, platform = platform )
 
 descriptor = {
 	'urls'			: [	'http://www.libsdl.org/release/SDL-1.2.15.tar.gz',
-						'http://orange/files/Dev/localExt/PUBLIC/src/SDL-1.2.15_patch.zip'
+						'$SRCPAKPATH/SDL-1.2.15_patch.zip' 
 		],
 
 	'name'			: descriptorName,
 	'version'		: descriptorVersion,
 
 	'rootBuildDir'	: projectFolderName,
-	'builds'		: [ cmdSDLRelease, cmdSDLMainRelease ],
+	'builds'		: [	# Fixes vcproj files
+						SearchVcxproj(vcxprojFiles, slnPath),
+						# Adds <AdditionalLibraryDirectories>dxguidPath</AdditionalLibraryDirectories>
+# @todo helper in sbfPackagingSystem.py
+						Patcher(vcxprojFiles, [('^(.*\<Link\>.*)$', '\\1\n      <AdditionalLibraryDirectories>{}</AdditionalLibraryDirectories>'.format(dxguidPath))]),
+						#	Configures which version of cl have to be invoke.
+						MSVC['SetPlatformToolset']( vcxprojFiles, CCVersionNumber ),
+						# build
+						cmdSDLRelease, cmdSDLMainRelease ],
 
 	# packages developer and runtime
 	'rootDir'		: projectFolderName,
@@ -52,12 +63,4 @@ descriptor = {
 	# runtime package
 	'bin'				: ['VisualC/SDL/Release/SDL.dll'],
 	#'runtimeCustom'	: [],
-
-	# runtime package (release version)
-	#'binR'			: [],
-	#'runtimeCustomR'	: [],
-
-	# runtime package (debug version)
-	#'binD'			: [],
-	#'runtimeCustomD'	: [],
 }
