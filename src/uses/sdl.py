09b56b43795947833dd827cdeec6ec83ec34a0c3
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2014-11-26 09:00:37 +0000

    ~ Updates uses='sdl' to version 2-0-3 and to support emscripten toolchain.

--- src/uses/sdl.py
+++ src/uses/sdl.py
@@ -8,12 +8,11 @@ class Use_sdl( IUse ):
 		return 'sdl'
 
 	def getVersions( self ):
-		#return ['2-0-3', '1-2-15', '1-2-14']
-		return ['1-2-15', '2-0-3', '1-2-14']
+		return ['2-0-3', '1-2-15', '1-2-14']
 
 	def getRequirements( self, version ):
-		if version == '2-0-3':
-			return ['angle 2-0-0']
+		if version == '2-0-3' and self.platform == 'win' and self.cc != 'emcc':
+			return ['angle']
 		else:
 			return []
 
@@ -23,17 +22,23 @@ class Use_sdl( IUse ):
 		else:
 			return []
 
+	def getCPPFLAGS( self, version ):
+		if self.cc == 'emcc' and version == '2-0-3':
+			return ['-s', 'USE_SDL=2']
+		return []
+
 	def getCPPPATH( self, version ):
-		if self.platform == 'posix' :
-			return ['SDL']
-		else:
+		if self.platform == 'win':
 			if version == '2-0-3':
 				return ['SDL2']
 			else:
 				return []
+		elif self.platform == 'posix':
+			return ['SDL']
 
 
 	def getLIBS( self, version ):
+		if self.cc == 'emcc':	return [], []
 		if self.platform == 'win' :
 			if version == '2-0-3':
 				libs = [ 'SDL2', 'SDL2main' ]
@@ -43,7 +48,13 @@ class Use_sdl( IUse ):
 				pakLibs = [ 'SDL' ]
 				return libs, pakLibs
 		elif self.platform == 'posix':
-			return ['SDL', 'SDL']
+			return ['SDL'], []
+
+	def getLINKFLAGS( self, version ):
+		if self.cc == 'emcc' and version == '2-0-3':
+			return ['-s', 'USE_SDL=2']
+		return []
+
 
 	def hasRuntimePackage( self, version ):
-		return True
+		return self.cc != 'emcc'
