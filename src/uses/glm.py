e5f1d6da1aad92b7a261e2a3c43fd96a73674bd0
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2014-12-12 08:59:18 +0000

    ~ Updates uses='glm' to version 0-9-5-4.

--- src/uses/glm.py
+++ src/uses/glm.py
@@ -8,7 +8,11 @@ class Use_glm( IUse ):
 		return 'glm'
 
 	def getVersions( self ):
-		return [ '0-9-4-1', '0-9-3-4', '0-9-3-3', '0-8-4-1' ]
+		return [ '0-9-5-4' ,'0-9-4-1', '0-9-3-4', '0-9-3-3', '0-8-4-1' ]
+
+	def getCPPDEFINES( self, version ):
+		return ['GLM_FORCE_RADIANS']
+		# GLM_PRECISION_HIGHP_FLOAT ?
 
 	def hasRuntimePackage( self, version ):
-		return self.platform == 'posix'
+		return self.platform == 'posix' or self.cc == 'emcc'
