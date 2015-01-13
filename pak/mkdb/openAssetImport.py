5488b27184de222115a4546d7f6fda222c5b486a
npapier <npapier@dc778481-724a-0410-bf50-2515cb9dd570>
2015-01-13 13:49:07 +0000

    + Adds support for emcc in openAssetImport mkpak. + Adds support for emcc in physFS mkpak.

--- pak/mkdb/openAssetImport.py
+++ pak/mkdb/openAssetImport.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 
-# SConsBuildFramework - Copyright (C) 2012, 2014, Nicolas Papier.
+# SConsBuildFramework - Copyright (C) 2012, 2014, 2015, Nicolas Papier.
 # Distributed under the terms of the GNU General Public License (GPL)
 # as published by the Free Software Foundation.
 # Author Guillaume Brocker
 # Author Nicolas Papier
 
-# cl8, 9, 10, 11, 12 [Exp] and linux
+# cl8, 9, 10, 11, 12 [Exp], emcc and linux
 
 descriptorName = 'openassetimport'
 descriptorVersion = '3-0'
 svnRevision = 1270
 descriptorVersionDot = descriptorVersion.replace('-', '.')
-
-print ('INFORMATIONS: boost have to be installed in localExt')
 
 import os.path
 from src.sbfCMake import getCMakeCmdConfigure, getCMakeCmdBuildDebug, getCMakeCmdBuildRelease
@@ -21,15 +19,48 @@ from src.sbfCMake import getCMakeCmdConfigure, getCMakeCmdBuildDebug, getCMakeCm
 assimpRootBuildDir = 'assimp--{dotVersion}.{svnRevision}-source-only'.format( dotVersion=descriptorVersionDot, svnRevision=svnRevision )
 absRootBuildDir = os.path.join( buildDirectory, descriptorName + descriptorVersion, assimpRootBuildDir )
 
+RequiredProgram('cmake')
+RequiredLibrary('boost')
+
+builds = []
+if svnRevision == 1270:
+	builds += [	# Patch for version 3.0
+				# 198: return Couple<T>(db).MustGetObject(To<EXPRESS::ENTITY>())->To<T>();
+				#      return Couple<T>(db).MustGetObject(To<EXPRESS::ENTITY>())->template To<T>();
+				Patcher('code/StepFile.h', [('\-\>To\<T\>\(\)', '->template To<T>()')]),
+				# 204:	return e?Couple<T>(db).MustGetObject(*e)->ToPtr<T>():(const T*)0; =>
+				#		return e?Couple<T>(db).MustGetObject(*e)->template ToPtr<T>():(const T*)0;
+				Patcher('code/StepFile.h', [('\-\>ToPtr\<T\>\(\)\:', '->template ToPtr<T>():')])]
+# else pass
+
 if platform == 'win':
-	lib = [	# debug
-			'lib/Debug/assimpD.lib', 'bin/Debug/*.pdb',
-			# release
-			'lib/Release/assimp.lib' ]
-	binR = ['bin/Release/*.dll']
-	binD = ['bin/Debug/*.dll']
-	options =  ' -D BOOST_ROOT="{localExt}/include" '.format( localExt=localExtDirectory )
+	options =  ' -D Boost_INCLUDE_DIR="{localExt}/include" '.format( localExt=localExtDirectory )
+	options += ' -D BUILD_ASSIMP_TOOLS=OFF ' # so dx9 is no more needed
+	if CC == 'cl':
+		builds += [getCMakeCmdConfigure(CC, CCVersionNumber, arch, options), getCMakeCmdBuildDebug(), getCMakeCmdBuildRelease()]
+		lib = [	# debug
+				'lib/Debug/assimpD.lib', 'bin/Debug/*.pdb',
+				# release
+				'lib/Release/assimp.lib' ]
+		binR = ['bin/Release/*.dll']
+		binD = ['bin/Debug/*.dll']
+	elif CC == 'emcc':							# @todo debug version
+		options += ' -D BUILD_STATIC_LIB=ON '	# @todo dont work -D CMAKE_DEBUG_POSTFIX=-d
+		builds += [	#
+					getCMakeCmdConfigure(CC, CCVersionNumber, arch, options),
+					# debug
+					#getCMakeCmdBuildDebug(),
+					#
+					getCMakeCmdBuildRelease()]
+		lib = [	# debug
+				#'lib/libassimp-d.a', 'lib/libz-d.a',
+				# release
+				'lib/libassimp.a', 'lib/libz.a' ]
+		binR = []
+		binD = []
+
 elif platform == 'posix':
+	builds += [getCMakeCmdConfigure(CC, CCVersionNumber, arch, options), getCMakeCmdBuildDebug(), getCMakeCmdBuildRelease()]
 	lib  = []
 	binR = ['lib/libassimp.so*'] # @todo Improve this since symbolic links to the same library file will result in copies in the final zip archive
 	binD = []
@@ -38,28 +69,27 @@ else:
 	print >>sys.stderr, "Platform {} not supported".format(platform)
 	exit(1)
 
-options += ' -D BUILD_ASSIMP_TOOLS=OFF ' # so dx9 is no more needed
 
 # @todo parallel build
 
 descriptor = {
 
-	'urls':		[	'http://sourceforge.net/projects/assimp/files/assimp-{dotVersion}/assimp--{dotVersion}.{svnRevision}-source-only.zip'.format(dotVersion=descriptorVersionDot, svnRevision=svnRevision),
-					'$SRCPAKPATH/assimp--3.0.1270-source-only-patch.zip' ],
+	'urls'			:	[	'http://sourceforge.net/projects/assimp/files/assimp-{dotVersion}/assimp--{dotVersion}.{svnRevision}-source-only.zip'.format(dotVersion=descriptorVersionDot, svnRevision=svnRevision),
+							'$SRCPAKPATH/assimp--3.0.1270-source-only-patch.zip' ],
 
-	'name'		:	descriptorName,
-	'version'	:	descriptorVersion + '-2',
+	'name'			:	descriptorName,
+	'version'		:	descriptorVersion + '-2',
 
-	'rootBuildDir'	: assimpRootBuildDir,
-	'builds'		: [ getCMakeCmdConfigure(CC, CCVersionNumber, arch, options), getCMakeCmdBuildDebug(), getCMakeCmdBuildRelease() ],
+	'rootBuildDir'	:	assimpRootBuildDir,
+	'builds'		:	builds,
 
 	#
-	'rootDir'		: assimpRootBuildDir,
+	'rootDir'		:	assimpRootBuildDir,
 
 	# developer package
 	'license'		:	[ 'LICENSE' ],
 
-	'include'		: [ ('include/assimp/', 'assimp/') ],
+	'include'		:	[ ('include/assimp/', 'assimp/') ],
 
 	'lib'			:	lib,
 
--- pak/mkdb/physfs.py
+++ pak/mkdb/physfs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# SConsBuildFramework - Copyright (C) 2010, 2011, 2013, 2014, Nicolas Papier.
+# SConsBuildFramework - Copyright (C) 2010, 2011, 2013, 2014, 2015, Nicolas Papier.
 # Distributed under the terms of the GNU General Public License (GPL)
 # as published by the Free Software Foundation.
 # Author Nicolas Papier
@@ -8,7 +8,7 @@
 
 # http://icculus.org/physfs/
 
-# cl8-0[Exp], cl9-0[Exp], cl10-0[Exp] and cl11-0[Exp] (32/64bits) and gcc
+# cl8-0[Exp], cl9-0[Exp], cl10-0[Exp], cl11-0[Exp] and cl12-0[Exp] (32/64bits), emcc and gcc
 # Support for zip (zlib123), 7z (using lzma)
 
 import os.path
@@ -19,24 +19,39 @@ descriptorVersionWithDot = descriptorVersion.replace('-', '.')
 
 from src.sbfCMake import getCMakeCmdConfigure, getCMakeCmdBuildDebug, getCMakeCmdBuildRelease
 
-options = '-D PHYSFS_ARCHIVE_GRP:BOOL=OFF -D PHYSFS_ARCHIVE_HOG:BOOL=OFF -D PHYSFS_ARCHIVE_MVL:BOOL=OFF -D PHYSFS_ARCHIVE_QPAK:BOOL=OFF -D PHYSFS_ARCHIVE_WAD:BOOL=OFF -D PHYSFS_BUILD_STATIC:BOOL=OFF'
+RequiredProgram('cmake')
+
+# @todo -DPHYSFS_HAVE_CDROM_SUPPORT:BOOL=OFF
+options = ' -D PHYSFS_ARCHIVE_GRP:BOOL=OFF -D PHYSFS_ARCHIVE_HOG:BOOL=OFF -D PHYSFS_ARCHIVE_MVL:BOOL=OFF -D PHYSFS_ARCHIVE_QPAK:BOOL=OFF -D PHYSFS_ARCHIVE_WAD:BOOL=OFF -D PHYSFS_BUILD_TEST=OFF '
 options += ' -D CMAKE_DEBUG_POSTFIX=-d '
-cmdConfigure = getCMakeCmdConfigure(CC, CCVersionNumber, arch, options)
 
 if platform == 'win':
-	lib = ['debug/physfs-d.lib ', 'release/physfs.lib']
-	binR = ['release/physfs.dll']
-	binD = ['debug/physfs-d.dll']
+	if CC == 'cl':
+		options += ' -D PHYSFS_BUILD_STATIC=OFF -DPHYSFS_BUILD_SHARED=ON '
+		cmdConfigure = getCMakeCmdConfigure(CC, CCVersionNumber, arch, options)
+		builds = [ cmdConfigure, getCMakeCmdBuildDebug(), getCMakeCmdBuildRelease() ]
+		lib = ['debug/physfs-d.lib ', 'release/physfs.lib']
+		binR = ['release/physfs.dll']
+		binD = ['debug/physfs-d.dll']
+	elif CC == 'emcc':
+		options += ' -D PHYSFS_BUILD_STATIC=ON -DPHYSFS_BUILD_SHARED=OFF '
+		cmdConfigure = getCMakeCmdConfigure(CC, CCVersionNumber, arch, options)
+		builds = [	cmdConfigure, getCMakeCmdBuildRelease() ] # @todo getCMakeCmdBuildDebug(),
+		lib = ['libphysfs.a'] # @todo 'libphysfs-d.a'
+		binR = []
+		binD = []
 else:
 	lib = []
 	binR = ['*.so.*']
 	binD = []
 
+
+
 descriptor = {
  'urls'			: [ 'http://icculus.org/physfs/downloads/physfs-2.0.2.tar.gz' ],
 
  'rootBuildDir'	: descriptorName + '-' + descriptorVersionWithDot,
- 'builds'		: [ cmdConfigure, getCMakeCmdBuildDebug(), getCMakeCmdBuildRelease() ],
+ 'builds'		: builds,
 
  'name'			: descriptorName,
  'version'		: descriptorVersion,
